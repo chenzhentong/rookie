3369bbeb669edbdc162969cd061487746673a795
幸运的这就是me <1765770246@qq.com>
2021-05-24 21:08:40 +0800

    v1.0

--- src/sql/logistics.sql
+++
@@ -1,8 +0,0 @@
-create table logistics
-(
-    id int not null primary key auto_increment,
-    message varchar(256) not null,
-    sentDate datetime not null,
-    packageId int not null,
-    foreign key(packageId) references package(id)
-)
